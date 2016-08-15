d793ad0b622388adba8326c91f6848d4f72e0f20
Nina Satragno <nsatragno@gmail.com>
2016-08-15 15:52:55 -0300

    Menu.

--- app/sprites.rb
+++ app/sprites.rb
@@ -1,6 +1,7 @@
 # Preloads and shares sprites.
 
 module Sprites
+  MENU = Gosu::Image.new "assets/menu.png"
   PIXEL = Gosu::Image.new "assets/pixel.png"
   PLAYER = Gosu::Image.new "assets/player.png"
   WALL = Gosu::Image.new "assets/wall.png", :tileable => true
